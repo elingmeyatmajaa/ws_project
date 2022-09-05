4e1bf0d7836b77bd0e6bcd379ff85780bbd71360
Eling Meyatmaja <eling.atma@gmail.com>
2022-09-05 15:48:09 +0700

    update web scrapping

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import If
 import requests
 from bs4 import BeautifulSoup as bs
 
@@ -5,30 +6,14 @@ from app import URL
 
 URL = "https://id.77577.live/id/post/index"
 
-req = requests.get(URL)
 
-soup = bs(req.text, 'html.parser')
+for page in range(1,10):
+    req = requests.get(URL)
+    soup = bs(req.text, 'html.parser')
+    titles = soup.find_all('div', attrs = {'class', 'row p-0 m-0'})
 
-titles = soup.find_all('div', attrs= {'class', 'row p-0 m-0'})
-print(titles[1].text)
-# r =requests.get('https://id.77577.live/id/post/index')
-
-
-# soup = BeautifulSoup(r.content, 'html.parser')
-
-# for link in soup.find_all('a'):
-#     print(link.get('href'))
-
-# s = soup.find("div", class_="row p-0 m-0 mb-3")
-
-
-# contents = s.find_all( 'p')
-
-# for content in contents:
-#     print(content.text)
-
-# print(soup.title)
-
-# print(soup.title.name)
-
-# print(soup.title.parent.name)
+    for i in range(4, 19):
+        if page > 1:
+            print(f"{(i-3)+page*15}" + titles[i].text)
+        else:
+            print(f"{i-3}" + titles[i].text)
--- web-s.py
+++ web-s.py
@@ -1,15 +1,16 @@
 import requests
 from bs4 import BeautifulSoup as bs
  
-URL = 'https://www.geeksforgeeks.org/page/'
+URL = ['https://www.geeksforgeeks.org', 'https://www.geeksforgeeks.org/page/10/']
 
-for page in range(1,10):
-    req = requests.get(URL + str(page) + '/')
+for url in range(0,2):
+    req = requests.get(URL[url])
     soup = bs(req.text, 'html.parser')
-    titles = soup.find_all('div',attrs = {'class','head'})
 
-    for i in range(4, 19):
-        if page > 1: 
-            print(f"{(i-3)+page*15}" + titles[i].text)
-        else: 
+    titles = soup.find_all('div', attrs={'class', 'head'})
+
+    for i in range(4,19):
+        if url+1 > 1:
+            print(f"{(i-3) + url * 15}" + titles[i].text)
+        else:
             print(f"{i-3}" + titles[i].text)
