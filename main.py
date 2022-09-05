80d853267788ed98af8b7d4f3e3a903139cf72fe
Eling Meyatmaja <eling.atma@gmail.com>
2022-09-05 13:58:39 +0700

    update link href in web target

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ r =requests.get('https://id.77577.live/id/post/index')
 
 soup = BeautifulSoup(r.content, 'html.parser')
 
-s = soup.find("div", class_="row p-0 m-0 mb-3")
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+# s = soup.find("div", class_="row p-0 m-0 mb-3")
 
 
-contents = s.find_all( 'p')
+# contents = s.find_all( 'p')
 
-for content in contents:
-    print(content.text)
+# for content in contents:
+#     print(content.text)
 
 # print(soup.title)
 
