6137f5e2aa3c1736c4a466719823c6678d06da0c
Eling Meyatmaja <eling.atma@gmail.com>
2022-11-12 10:18:39 +0700

    update video

---
+++ web_sport_es_api_football.py
@@ -0,0 +1,91 @@
+import time
+from urllib import response
+from numpy import tile
+import requests
+from bs4 import BeautifulSoup
+import json
+import string
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pprint import pprint
+import csv
+from datetime import datetime
+
+
+    
+
+
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument("--headless")
+options.add_argument('--disable-dev-shm-usage')
+
+import mysql.connector
+import mysql.connector as mysql
+
+import html
+
+mydb = mysql.connect(
+        host="localhost",
+        user="root",
+        password="strong_password1234",
+        database="db_test_allfootball"
+)
+
+# driver = webdriver.Chrome('./chromedriver', options = options)
+
+base = "https://www.sport.es/"
+# browser = webdriver.Chrome('/home/eling/chromedriver', options = options)
+browser = webdriver.Chrome('D:/REPOSITORY/python_project/driver/chromedriver.exe')
+wait = WebDriverWait(browser, 10)
+browser.get('https://www.sport.es/en')
+
+
+soup = BeautifulSoup(browser.page_source,'lxml')
+
+links = soup.find('h2', {'class':'title'})
+link_url = links.a['href']
+
+response = requests.get(link_url)
+
+
+soup_link = BeautifulSoup(response.content, 'html5lib')
+
+table = soup_link.find('article', {'class':'newdetail'})
+
+tables_1 = soup_link.find('div', {'class':'col-xs-12 col-sm-12 col-md-8 col-lg-8 col'})
+
+a = '1'
+b = table.img['src']
+c = tables_1.figure.div['data-video-file']
+d = table.h1.text
+e = ' '
+f = ' '
+g = ' '
+h = '1'
+i = datetime.now()
+
+
+# print(a,b,c,d,e,f,g,h,i)
+
+
+mycursor = mydb.cursor()
+
+
+query = "INSERT INTO videos(id_kategori_video,covers, file, description, descriptionId, descriptionVi, descriptionCn,user_id, created_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+values = (a,b,c,d,e,f,g,h,i)
+
+mycursor.execute(query, values)
+
+mydb.commit()
+print(mycursor.rowcount, "was inserted.")
+
+print("Complete")
+
+browser.quit()
